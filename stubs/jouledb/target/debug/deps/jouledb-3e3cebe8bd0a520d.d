2bfe2fa50aa87bd751d536fe3c5206422b2fc93e
David Charlot <david@openie.dev>
2026-06-08 09:46:47 -0400

    stub: switch from `license = "BSL-1.1"` to `license-file = "LICENSE"`
    crates.io's manifest validator only accepts SPDX-listed licenses for
    the `license` field. BSL-1.1 isn't on SPDX. Use `license-file` and
    bundle the LICENSE file alongside the stub so cargo publish accepts
    it.

---
+++ stubs/jouledb/target/debug/deps/jouledb-3e3cebe8bd0a520d.d
@@ -0,0 +1,9 @@
+/private/tmp/openie-public-deploy/clones/jouledb/stubs/jouledb/target/debug/deps/jouledb-3e3cebe8bd0a520d.d: src/main.rs
+
+/private/tmp/openie-public-deploy/clones/jouledb/stubs/jouledb/target/debug/deps/jouledb-3e3cebe8bd0a520d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CARGO_PKG_NAME=jouledb
+# env-dep:CARGO_PKG_REPOSITORY=https://github.com/openIE-dev/jouledb
+# env-dep:CARGO_PKG_VERSION=0.1.0
---
+++ stubs/jouledb/target/debug/jouledb.d
@@ -0,0 +1,1 @@
+/private/tmp/openie-public-deploy/clones/jouledb/stubs/jouledb/target/debug/jouledb: /private/tmp/openie-public-deploy/clones/jouledb/stubs/jouledb/target/package/jouledb-0.1.0/src/main.rs
